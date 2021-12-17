5fafedb28fb88d358d8144d9653a4454504d81f8
fv0 <fv0@users.noreply.github.com>
2021-12-17 15:11:48 +0100

    The shit works!

--- index.js
+++ index.js
@@ -2,28 +2,21 @@ import puppeteer from "puppeteer";
 import * as fs from "fs";
 import fetch from "node-fetch";
 
-const urlAllProducts =
-  "https://www.bucherer.com/ch/de/buy-certifiedpreowned?srule=searching-result-sorting&start=0&sz=";
-const amountOfProductsToScrape = 5;
-const scrapeUrl = urlAllProducts + amountOfProductsToScrape;
+(async () => {
+  // Helper URLs
+  const urlAllProducts =
+    "https://www.bucherer.com/ch/de/buy-certifiedpreowned?srule=searching-result-sorting&start=0&sz=";
+  const amountOfProductsToScrape = 100;
+  const scrapeUrl = urlAllProducts + amountOfProductsToScrape;
 
-async function getAvailabilities(url) {
-  const response = await fetch(url);
-  const data = await response.json();
-
-  for (var i = 0; i < data.stores.length; i++) {
-    if (data.stores[i].availability.inStock == true) {
-      console.log("Available in " + data.stores[i].name);
-    }
-  }
-}
-
-async function scrape(url) {
+  // Setup Puppeteer
   const browser = await puppeteer.launch();
   const page = await browser.newPage();
-  await page.goto(url);
+  await page.goto(scrapeUrl);
 
-  var watches = await page.evaluate(() => {
+  var watches = await page.evaluate(async () => {
+
+    // Collect information about all watches
     var productList = document.querySelectorAll(".product-grid .product");
     var brand = document.querySelectorAll(".m-product-tile__product-brand");
     var model = document.querySelectorAll(".m-product-tile__product-model");
@@ -33,27 +26,48 @@ async function scrape(url) {
     var watchArray = [];
 
     for (var i = 0; i < productList.length; i++) {
-      // Save the 
+
+      async function getAvailabilities(pid) {
+        // Request availability for this watch in all stores based on the unique product ID (PID).
+        const response = await fetch(
+          "https://www.bucherer.com/on/demandware.store/Sites-bucherer-Site/de_CH/Store-Availability?pid=" +
+            pid
+        );
+        const data = await response.json();
+
+        let returnvalue = "Not available in stores.";
+
+        for (var i = 0; i < data.stores.length; i++) {
+          // Check which store has this watch in stock
+          if (data.stores[i].availability.inStock === true) {
+            returnvalue = data.stores[i].name;
+          }
+        }
+
+        return returnvalue;
+      }
+
+      // Get product ID
       const pid = productList[i].getAttribute("data-pid");
       
-      // URL to check the availability of a store
-      const urlStoreAvailability =
-        "https://www.bucherer.com/on/demandware.store/Sites-bucherer-Site/de_CH/Store-Availability?pid=" + pid;
-
       watchArray[i] = {
         brand: brand[i].innerText,
         model: model[i].innerText,
-        label: label[i].innerText,
+        // label: label[i].innerText,
         price: Number(price[i].getAttribute("content")) + " CHF",
-        pid: pid
+        pid: pid,
+        availableIn: await getAvailabilities(pid)
       };
+
+      // watchArray[i].availableIn = await getAvailabilities(pid);
+
     }
     return watchArray;
   });
-
+  
   const exportFileName = "watches.json";
 
-  // Write results into a JSON file
+  // Write array with watch information into a JSON file
   fs.writeFile(
     "./" + exportFileName,
     JSON.stringify(watches, null, 3),
@@ -67,7 +81,4 @@ async function scrape(url) {
   );
 
   browser.close();
-}
-
-scrape(scrapeUrl);
-getAvailabilities("https://www.bucherer.com/on/demandware.store/Sites-bucherer-Site/de_CH/Store-Availability?pid=1350-723-0");
+})();
